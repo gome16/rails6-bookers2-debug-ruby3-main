ddb39f586f9f708a57ae92e4345d6a520365e995
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-09-29 09:52:22 +0000

    [Fix] 全ページログイン制限からhomes_controllerページを除外

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root :to =>"homes#top"
+  root "homes#top"
+  get "homes/top", as: "top"
   get "home/about"=>"homes#about"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
